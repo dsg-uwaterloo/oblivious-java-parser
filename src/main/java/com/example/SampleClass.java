81958f623b9d4d52896221ef44bc7a737686e967
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2024-07-22 16:48:58 -0400

    Add loop test

--- src/main/java/com/example/SampleClass.java
+++ src/main/java/com/example/SampleClass.java
@@ -1,20 +1,26 @@
 package com.example;
 
 public class SampleClass {
+    public static void main(String[] args) {
+        String[] names = new String[2];
+        names[0] = "Aaryan Shroff";
+        names[1] = "John Doe";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
 
-  public static void main(String[] args) {
-    String[] names = new String[2];
-    names[0] = "Aaryan Shroff";
-    names[1] = "John Doe";
-    System.out.println(names[0]);
-    System.out.println(names[1]);
+        int[] date = new int[3];
+        date[0] = 18;
+        date[1] = 7;
+        date[2] = 2024;
+        System.out.println(date[0]);
+        System.out.println(date[1]);
+        System.out.println(date[2]);
 
-    int[] date = new int[3];
-    date[0] = 18;
-    date[1] = 7;
-    date[2] = 2024;
-    System.out.println(date[0]);
-    System.out.println(date[1]);
-    System.out.println(date[2]);
-  }
+        String[] sentence = new String[2];
+        sentence[0] = "Hello";
+        sentence[1] = "World";
+        for (int i = 0; i < sentence.length; i++) {
+            System.out.println(sentence[i]);
+        }
+    }
 }
