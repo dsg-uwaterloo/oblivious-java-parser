68c5129143b218eb7d5e33a757977d36395e94fb
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2024-07-25 18:29:53 -0400

    Sample Class with more cases

--- src/main/java/com/example/SampleClass.java
+++ src/main/java/com/example/SampleClass.java
@@ -1,36 +1,31 @@
 package com.example;
 
-import java.util.Optional;
-import java.nio.ByteBuffer;
-
 public class SampleClass {
-
     public static void main(String[] args) {
-        oram.access("names[" + String.valueOf(0) + "]", Optional.ofNullable("Aaryan Shroff".getBytes()), true);
-        oram.access("names[" + String.valueOf(1) + "]", Optional.ofNullable("John Doe".getBytes()), true);
-        System.out.println(
-                new String(oram.access("names[" + String.valueOf(0) + "]", Optional.<byte[]>empty(), false).get()));
-        System.out.println(
-                new String(oram.access("names[" + String.valueOf(1) + "]", Optional.<byte[]>empty(), false).get()));
-        oram.access("date[" + String.valueOf(0) + "]", Optional.ofNullable(ByteBuffer.allocate(4).putInt(18).array()),
-                true);
-        oram.access("date[" + String.valueOf(1) + "]", Optional.ofNullable(ByteBuffer.allocate(4).putInt(7).array()),
-                true);
-        oram.access("date[" + String.valueOf(2) + "]", Optional.ofNullable(ByteBuffer.allocate(4).putInt(2024).array()),
-                true);
-        System.out.println(ByteBuffer
-                .wrap(oram.access("date[" + String.valueOf(0) + "]", Optional.<byte[]>empty(), false).get()).getInt());
-        System.out.println(ByteBuffer
-                .wrap(oram.access("date[" + String.valueOf(1) + "]", Optional.<byte[]>empty(), false).get()).getInt());
-        System.out.println(ByteBuffer
-                .wrap(oram.access("date[" + String.valueOf(2) + "]", Optional.<byte[]>empty(), false).get()).getInt());
-        oram.access("sentence[" + String.valueOf(0) + "]", Optional.ofNullable("Hello".getBytes()), true);
-        oram.access("sentence[" + String.valueOf(1) + "]", Optional.ofNullable("World".getBytes()), true);
-        for (int i = 0; i < 2; i++) {
-            System.out.println(new String(
-                    oram.access("sentence[" + String.valueOf(i) + "]", Optional.<byte[]>empty(), false).get()));
+        if (args.length > 0) {
+            System.out.println(args[0]);
+            args[0] = "New value";
+            System.out.println(args[0]);
+        }
+        String[] names = new String[2];
+        names[0] = "Aaryan Shroff";
+        names[1] = "John Doe";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+
+        int[] date = new int[3];
+        date[0] = 18;
+        date[1] = 7;
+        date[2] = 2024;
+        System.out.println(date[0]);
+        System.out.println(date[1]);
+        System.out.println(date[2]);
+
+        String[] sentence = new String[2];
+        sentence[0] = "Hello";
+        sentence[1] = "World";
+        for (int i = 0; i < sentence.length; i++) {
+            System.out.println(sentence[i]);
         }
     }
-
-    private static PathORAM oram = new PathORAM(10000);
 }
